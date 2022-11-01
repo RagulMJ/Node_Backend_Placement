7f1b71b42f0c5438631fbfc1f7c48192f9ffd153
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2022-11-01 23:18:33 +0530

    server

--- server.js
+++ server.js
@@ -34,6 +34,5 @@ app.get('/', (req, res) => {
   res.send('web server running');
 });
 
-// console.log(Math.floor(Math.random() * 100000));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on ${port}`));
